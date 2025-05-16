e3a22235e3b561011979e5f51a6559e1f78835f4
Aleksandr <generalovshura9@gmail.com>
2025-05-16 19:32:06 +0300

    created first component for main page and add Service into a project

--- src/components/main-page/main-page.component.ts
+++ src/components/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-main-page',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit,OnDestroy {
+  public formControl: FormControl = new FormControl()
 
+  ngOnInit(): void {
+    this.formControl = new FormControl('')
+  }
+
+  ngOnDestroy(): void {
+
+  }
 }
